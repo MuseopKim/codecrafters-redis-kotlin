54bbaba8a540465f8644b45d473c29c512cd7e63
Museop <museopkim0214@gmail.com>
2025-09-14 22:50:09 +0900

    refactor: respond to pings

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,4 @@
 import java.net.ServerSocket
-import java.util.stream.IntStream
 
 fun main(args: Array<String>) {
     var serverSocket = ServerSocket(6379)
@@ -10,27 +9,15 @@ fun main(args: Array<String>) {
     val clientSocket = serverSocket.accept()
     println("accepted new connection")
 
-    try {
-        while (true) {
-            val reader = clientSocket.inputStream.bufferedReader()
-            val writer = clientSocket.outputStream.bufferedWriter()
+    val reader = clientSocket.inputStream.bufferedReader()
+    val writer = clientSocket.outputStream.bufferedWriter()
 
-            val multipleCommands: MutableList<String> = mutableListOf();
-            val commandCountRESP = reader.readLine() ?: return
-            val commandCount = commandCountRESP.split("*")[1].toLong()
-            println("Command count: $commandCount")
-            (1..commandCount).forEach { _ ->
-                val characterCount = reader.readLine().split("$")[1].toLong()
-                val command = reader.readLine()
-                println("$command $characterCount")
-                multipleCommands.add(command)
+    while (!clientSocket.isClosed) {
+        reader.forEachLine {
+            if (it.startsWith("PING")) {
+                writer.write("+PONG\r\n")
+                writer.flush()
             }
-
-
-            (1..multipleCommands.size).forEach { _ -> writer.write("+PONG\r\n") }
-            writer.flush()
         }
-    } finally {
-        clientSocket.close()
     }
 }
