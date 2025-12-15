af311ea4cb370668e8adf12fe246c5cca5916424
Museop <museopkim0214@gmail.com>
2025-12-15 23:03:44 +0900

    feat: configure listening port

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,11 @@
-import store.RedisDataStore
-import java.net.ServerSocket
-
 fun main(args: Array<String>) {
-    var serverSocket = ServerSocket(6379)
-    // Since the tester restarts your program quite often, setting SO_REUSEADDR
-    // ensures that we don't run into 'Address already in use' errors
-    serverSocket.reuseAddress = true
-
-    val dataStore = RedisDataStore()
-
-    while (true) {
-        val clientSocket = serverSocket.accept()
-
-        val thread = Thread(Session(clientSocket, dataStore))
-        thread.start()
+    val portArgumentIndex = args.indexOfFirst { it == "--port" }
+    val port = if (portArgumentIndex != -1 && args.size > portArgumentIndex + 1) {
+        args[portArgumentIndex + 1].toIntOrNull() ?: 6379
+    } else {
+        6379
     }
+
+    val redisServer = RedisServer(host = "localhost", port = port)
+    redisServer.run()
 }
